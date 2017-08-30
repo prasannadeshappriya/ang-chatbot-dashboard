651fd87073e8d4137a42cb516085469121878579
Prasanna Deshappriya <prasanna.14@cse.mrt.ac.lk>
2017-08-30 17:38:57 +0530

    Minor changes

--- app/controller/main.controller.js
+++ app/controller/main.controller.js
@@ -4,10 +4,19 @@
 app.controller('mainController',[
     '$scope', '$http',
     function ($scope,$http) {
+        //-----------------------Alert Show Section-----------------------
+        $scope.dangerAlert = false;
+        $scope.successAlert = false;
+        $scope.message = '';
+        //-----------------------On Loading-------------------------------
+        $scope.onInit = function () {
+            console.log('Initializing variables');
+        };
+        //-----------------------Views Section----------------------------
         $scope.showEntitySection = true;
         $scope.showSettingsSection = false;
         $scope.viewController = {
-            entity: true, settings: false
+            intent: true, settings: false, entity: false
         };
         $scope.showView = function (view_name) {
             Object.keys($scope.viewController)
@@ -16,5 +25,14 @@ app.controller('mainController',[
                     else{$scope.viewController[key]=false;}
                 });
         };
+        //----------------------------------------------------------------
+        $scope.appEntities = [
+            {name: 'leave'}, {name: 'funny'}
+        ];
+        $scope.appIntents = [
+            {name: 'leave'}, {name: 'funny'}
+        ];
+        if($scope.appIntents.length>0){$scope.selectedIntentName = $scope.appIntents[0].name;}
+        if($scope.appEntities.length>0){$scope.selectedEntityName = $scope.appEntities[0].name;}
     }
 ]);
