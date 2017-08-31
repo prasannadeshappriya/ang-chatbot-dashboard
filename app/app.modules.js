7859cdd1dc8fe335a03b8114bbb31e8fab9c198c
Prasanna Deshappriya <prasanna.14@cse.mrt.ac.lk>
2017-08-31 18:39:59 +0530

    Minor changes

--- app/app.modules.js
+++ app/app.modules.js
@@ -1,4 +1,8 @@
 /**
  * Created by prasanna_d on 8/30/2017.
  */
-let app = angular.module('chat-bot-app',['ngRoute','ui.router']);
+let app = angular.module('chat-bot-app',[
+    'ngRoute','ui.router'
+    ]).constant(
+        'host_url', 'http://localhost:4000/'
+);
--- app/controller/main.controller.js
+++ app/controller/main.controller.js
@@ -2,8 +2,8 @@
  * Created by prasanna_d on 8/30/2017.
  */
 app.controller('mainController',[
-    '$scope', '$http',
-    function ($scope,$http) {
+    '$scope', '$http', 'host_url',
+    function ($scope,$http,host_url) {
         //-----------------------Alert Show Section-----------------------
         $scope.dangerAlert = false;
         $scope.successAlert = false;
@@ -34,5 +34,43 @@ app.controller('mainController',[
         ];
         if($scope.appIntents.length>0){$scope.selectedIntentName = $scope.appIntents[0].name;}
         if($scope.appEntities.length>0){$scope.selectedEntityName = $scope.appEntities[0].name;}
+        //-----------------------Data Validation Section----------------------------
+        $scope.isSubmit = false;
+        $scope.intent_name_empty = false;
+        $scope.intent_name_dublicate = false;
+        $scope.intent_description_empty = false;
+        $scope.changeText = function (field) {
+            switch (field){
+                case 'intent_name_empty':
+                    $scope.intent_name_empty = false;
+                    break;
+                case 'intent_name_dublicate':
+                    $scope.intent_name_dublicate = false;
+                    break;
+                case 'intent_description_empty':
+                    $scope.intent_description_empty = false;
+                    break;
+            }
+        };
+        //----------------------------------------------------------------
+        $scope.createNewIntent = async function () {
+            console.log('create new intent');
+            $scope.isSubmit = true;
+            let con = true;
+            if($scope.intentName==='' || typeof $scope.intentName==='undefined'){$scope.intent_name_empty = true; con = false;}
+            if($scope.indentDes==='' || typeof $scope.indentDes==='undefined'){$scope.intent_description_empty = true; con = false;}
+            if(con){
+                try {
+                    let result = await $http({
+                        method: "POST",
+                        url: host_url + "intent/create",
+                        data: {intent_name: $scope.intentName, intent_description: $scope.indentDes}
+                    });
+                    console.log(result);
+                }catch (err){
+                    console.log(err);
+                }
+            }
+        };
     }
 ]);
