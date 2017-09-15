8b044c981568f4f63a612dacaf023cafa20227f3
3LOGIXS\prasanna_d <prasanna.14@cse.mrt.ac.lk>
2017-09-15 12:34:10 +0530

    Minor changes

--- app/controller/broadcast.message.controller.js
+++ app/controller/broadcast.message.controller.js
@@ -2,20 +2,29 @@
  * Created by prasanna_d on 9/15/2017.
  */
 app.controller('BroadcastMessage',[
-    '$scope','$http','host_url',
-    function ($scope,$http,host_url) {
+    '$scope','$http','host_url','AuthService','$location',
+    function ($scope,$http,host_url,AuthService,$location) {
         $scope.count = 0;
         $scope.isSend = false;
         $scope.sendMessage = async function () {
-            let result = await $http({
-                method: "POST",
-                url: host_url + "broadcast",
-                data: 'message=' + $scope.message,
-                headers: {'Content-Type': 'application/x-www-form-urlencoded'}
-            });
-            $scope.isSend = true;
-            $scope.count = result.data.count;
-            $scope.$apply();
+            try{
+                let result = await $http({
+                    method: "POST",
+                    url: host_url + "broadcast",
+                    data: 'message=' + $scope.message,
+                    headers: {'Content-Type': 'application/x-www-form-urlencoded'}
+                });
+                $scope.isSend = true;
+                console.log('Message sent to :');
+                console.log(result.data.sent_users);
+                $scope.count = result.data.sent_users.length;
+                $scope.$apply();
+            }catch (err){
+                if(err.status===401){
+                    AuthService.Logout();
+                    $location.path('/');
+                }
+            }
         }
     }
 ]);
