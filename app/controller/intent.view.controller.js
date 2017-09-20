166e52afe070919542c9d58b44aa26b5789bf064
3LOGIXS\prasanna_d <prasanna.14@cse.mrt.ac.lk>
2017-09-20 14:02:04 +0530

    Fix bugs in intent view

--- app/controller/intent.view.controller.js
+++ app/controller/intent.view.controller.js
@@ -63,9 +63,13 @@ app.controller('IntentController',[
         //-----------------------Data Validation Section----------------------------------------------------------------
         $scope.isSubmit = false;
         $scope.isLoading = true;
+        $scope.panel_loading = false;
+        $scope.isModelLoading = false;
         $scope.intent_name_empty = false;
         $scope.intent_name_dublicate = false;
         $scope.intent_description_empty = false;
+        $scope.intent_data_empty = false;
+        $scope.intent_model_data_empty = false;
         $scope.changeText = function (field) {
             switch (field){
                 case 'intent_name_empty':
@@ -77,30 +81,48 @@ app.controller('IntentController',[
                 case 'intent_description_empty':
                     $scope.intent_description_empty = false;
                     break;
+                case 'intent_data_empty':
+                    $scope.intent_data_empty = false;
+                    break;
+                case 'intent_model_data_empty':
+                    $scope.intent_model_data_empty = false;
+                    break;
             }
         };
         //------------------------Intent Update Model Section-----------------------------------------------------------
         $scope.selectedappIntents = {};
-        $scope.appIntentsItemClick = function (item) {
+        $scope.appIntentsItemClick = async function (item) {
+            $scope.isModelLoading = true;
             $scope.selectedappIntents = angular.copy(item);
             $scope.intUpdateExpression = '';
+            let result = await $http({
+                method: "GET",
+                url: host_url + "intent/get?intentid=" + $scope.selectedappIntents.value,
+                headers: {'Content-Type': 'application/x-www-form-urlencoded'}
+            });
+            $scope.isModelLoading = false;
+            if(result.data.data[1]) {
+                $scope.intentModelData = result.data.data[1].data;
+            }
+            $scope.$apply();
         };
         $scope.appIntentsItemDelete = async function () {
             let tmp_arr = [];
             for(let i=0; i<$scope.appIntents.length; i++ ){
                 if($scope.appIntents[i].value!==$scope.selectedappIntents.value){
                     tmp_arr.push($scope.appIntents[i]);
-                    break;
                 }
             }
             $scope.appIntents = tmp_arr;
+            $scope.isModelLoading = true;
+            try {
                 let result = await $http({
                     method: "POST",
                     url: host_url + "intent/delete",
-                    data: 'intent_name='+ $scope.selectedappIntents.value,
+                    data: 'intent_name=' + $scope.selectedappIntents.value,
                     headers: {'Content-Type': 'application/x-www-form-urlencoded'}
                 });
-                if(result.status===200) {
+                if (result.status === 200) {
                     let data = JSON.stringify($scope.appIntents, function (key, value) {
                         if (key === "$$hashKey") {
                             return undefined;
@@ -116,48 +138,97 @@ app.controller('IntentController',[
                         data: 'entity_name=' + 'intent' + '&wit_values=' + data + '&wit_doc=' + $scope.indentDes,
                         headers: {'Content-Type': 'application/x-www-form-urlencoded'}
                     });
-                    if(result.status===200) {
+                    if (result.status === 200) {
                         $scope.message = 'Intent successfully deleted!';
                         $scope.successAlert = true;
+                        $scope.isModelLoading = false;
                         $scope.$apply();
-                    }else{
+                    } else {
+                        $scope.isModelLoading = false;
                         $scope.message = 'Error occurred!';
                         $scope.dangerAlert = true;
                         $scope.$apply();
                     }
-                }else{
+                } else {
+                    $scope.isModelLoading = false;
                     $scope.message = 'Error occurred!';
                     $scope.dangerAlert = true;
                     $scope.$apply();
                 }
+            }catch (err){
+                $scope.isModelLoading = false;
+                $scope.message = 'Error occurred!';
+                $scope.dangerAlert = true;
+                $scope.$apply();
+            }
             $('#updateData').modal('hide');
         };
         $scope.appIntentsItemUpdate = async function () {
-            let tmp_arr = [];
-            for(let i=0; i<$scope.appIntents.length; i++ ){
-                if($scope.appIntents[i].value!==$scope.selectedappIntents.value){
-                    tmp_arr.push($scope.appIntents[i]);
-                    break;
-                }else{
-                    tmp_arr.push($scope.selectedappIntents);
-                }
+            let con = true;
+            $scope.intent_model_data_empty = false;
+            if(typeof $scope.intentModelData==='undefined' ||
+                    $scope.intentModelData === ''){
+                $scope.intent_model_data_empty = true; con = false;
             }
-            $scope.appIntents = tmp_arr;
-            let data = JSON.stringify( $scope.appIntents, function( key, value ) {
-                if( key === "$$hashKey" ) {return undefined;}
-                return value;
-            });
-            if(typeof $scope.indentDes==='undefined'){$scope.indentDes='';}
-            let result = await $http({
-                method: "POST",
-                url: host_url + "wit/putEntityById",
-                data: 'entity_name='+ 'intent'+ '&wit_values=' + data + '&wit_doc=' + $scope.indentDes,
-                headers: {'Content-Type': 'application/x-www-form-urlencoded'}
-            });
-            $scope.message = 'Intent successfully updated!';
-            $scope.successAlert = true;
-            $scope.$apply();
-            $('#updateData').modal('hide');
+            if(con) {
+                $scope.isModelLoading = true;
+                let tmp_arr = [];
+                for (let i = 0; i < $scope.appIntents.length; i++) {
+                    if ($scope.appIntents[i].value !== $scope.selectedappIntents.value) {
+                        tmp_arr.push($scope.appIntents[i]);
+                    } else {
+                        tmp_arr.push($scope.selectedappIntents);
+                    }
+                }
+                $scope.appIntents = tmp_arr;
+                let data = JSON.stringify($scope.appIntents, function (key, value) {
+                    if (key === "$$hashKey") {
+                        return undefined;
+                    }
+                    return value;
+                });
+                if (typeof $scope.indentDes === 'undefined') {
+                    $scope.indentDes = '';
+                }
+                try {
+                    let result = await $http({
+                        method: "POST",
+                        url: host_url + "intent/update",
+                        data: 'intent_name=' + $scope.selectedappIntents.value + '&intent_data=' + $scope.intentModelData,
+                        headers: {'Content-Type': 'application/x-www-form-urlencoded'}
+                    });
+                    if(result.data.data) {
+                        try {
+                            let result = await $http({
+                                method: "POST",
+                                url: host_url + "wit/putEntityById",
+                                data: 'entity_name=' + 'intent' + '&wit_values=' + data + '&wit_doc=' + $scope.indentDes,
+                                headers: {'Content-Type': 'application/x-www-form-urlencoded'}
+                            });
+                            $scope.isModelLoading = false;
+                            $scope.message = 'Intent successfully updated!';
+                            $scope.successAlert = true;
+                            $scope.$apply();
+                        }catch (err){
+                            $scope.isModelLoading = false;
+                            $scope.message = 'Internal server error';
+                            $scope.dangerAlert = true;
+                            $scope.$apply();
+                        }
+                    }else {
+                        $scope.isModelLoading = false;
+                        $scope.message = 'Something wrong!';
+                        $scope.dangerAlert = true;
+                        $scope.$apply();
+                    }
+                }catch (err){
+                    $scope.isModelLoading = false;
+                    $scope.message = 'Internal server error';
+                    $scope.dangerAlert = true;
+                    $scope.$apply();
+                }
+                $('#updateData').modal('hide');
+            }
         };
         $scope.appIntentsUpdateItemClick = function (item) {
             for(let i=0; i<$scope.selectedappIntents.expressions.length; i++){
@@ -180,24 +251,26 @@ app.controller('IntentController',[
         $scope.createNewIntent = async function () {
             resetAlert();
             $scope.isSubmit = true;
+            $scope.panel_loading = true;
             let con = true;
             if($scope.intentName==='' || typeof $scope.intentName==='undefined'){$scope.intent_name_empty = true; con = false;}
             if($scope.indentDes==='' || typeof $scope.indentDes==='undefined'){$scope.intent_description_empty = true; con = false;}
+            if($scope.intentData==='' || typeof $scope.intentData==='undefined'){$scope.intent_data_empty = true; con = false;}
             if(con){
                 try {
                     let result = await $http({
                         method: "POST",
-                        url: host_url + "intent/create&token=" + AuthService.getToken(),
-                        data: 'intent_name='+ $scope.intentName+ '&intent_description=' + $scope.indentDes,
+                        url: host_url + "intent/create",
+                        data: 'intent_name='+ $scope.intentName+ '&intent_description=' + $scope.indentDes +
+                                    '&intent_data=' + $scope.intentData,
                         headers: {'Content-Type': 'application/x-www-form-urlencoded'}
                     });
                     console.log(result);
                     if(result){
                         if(result.status===201){
-                            $scope.message = 'Intent successfully created!';
-                            $scope.successAlert = true;
                             $scope.appIntents.push({value: $scope.intentName, expressions: $scope.add_expressions});
                             $scope.selectedIntentName = $scope.intentName;
+
                             let data = JSON.stringify( $scope.appIntents, function( key, value ) {
                                 if( key === "$$hashKey" ) {return undefined;}
                                 return value;
@@ -211,22 +284,29 @@ app.controller('IntentController',[
                             });
                             if(result.status===200) {
                                 console.log(result);
-                                $scope.intentName = '';
+                                $scope.message = 'Intent successfully created!';
+                                $scope.successAlert = true;
+                                $scope.panel_loading = false;
+                                $scope.intentName = ''; $scope.intentData = '';
+                                $scope.add_expressions = [];
                                 $scope.$apply();
                             }else{
                                 $scope.message = 'Error occurred!';
+                                $scope.panel_loading = false;
                                 $scope.dangerAlert = true;
                                 $scope.$apply();
                             }
                         }else{
                             $scope.message = 'Error occurred!';
                             $scope.dangerAlert = true;
+                            $scope.panel_loading = false;
                             $scope.$apply();
                         }
                     }
                     console.log('Intent successfully created!');
                 }catch (err){
-                    if(err.status===409){$scope.intent_name_dublicate = true; $scope.$apply();}
+                    if(err.status===409){$scope.intent_name_dublicate = true;
+                        $scope.panel_loading = false; $scope.$apply();}
                     // console.log(err);
                 }
             }
